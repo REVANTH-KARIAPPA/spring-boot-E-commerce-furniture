da4e88a22ef4a19849e4cc2dc7ab642951fa2c07
Revanth-Kariappa <revanthkariappa4@gmail.com>
2022-09-14 18:59:27 +0530

    9 commit

--- src/main/java/com/furniture/service/PaymentService.java
+++ src/main/java/com/furniture/service/PaymentService.java
@@ -6,7 +6,7 @@ import java.util.List;
 import com.furniture.exception.PaymentNotFoundException;
 import com.furniture.model.Payment;
 
-public Interface PaymentService {
+public interface PaymentService {
 	public Payment addPayment(Payment payment,int pid,int orderId);
 	public boolean deletePaymentById(Integer paymentId);
 	public List<Payment> getAllPaymentDetails();
