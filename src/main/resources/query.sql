4bf5242824ee596ad21c91efbc11848b557aa627
Revanth Kariappa K R <83281653+REVANTH-KARIAPPA@users.noreply.github.com>
2022-09-20 16:20:58 +0530

    commit

--- src/main/resources/query.sql
+++ src/main/resources/query.sql
@@ -1,4 +1,3 @@
 INSERT INTO role (id, description, name) VALUES (1, 'Admin role', 'ADMIN');
 INSERT INTO role (id, description, name) VALUES (2, 'Customer role', 'CUSTOMER');
 INSERT INTO role (id, description, name) VALUES (3, 'Seller role', 'SELLER');
-
